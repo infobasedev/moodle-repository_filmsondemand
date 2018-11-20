d0c44e7cbfa52a2255dc96ea5c808db3b957e06b
Kenneth Scalia <kscalia@FOF2000.LOCAL>
2018-11-20 12:04:34 -0500

    adding files

---
+++ lang/en/repository_filmsondemand.php
@@ -0,0 +1,33 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'repository_filmsondemand', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   repository_filmsondemand
+ * @Author    Pramod Ubbala (AGS -> Infobase)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['configplugin'] = 'Films On Demand configuration';
+$string['licensekey'] = 'Access key';
+$string['launchURl'] =  'Launch URL';
+$string['pluginname_help'] = 'Films On Demand';
+$string['pluginname'] = 'Films On Demand';
+$string['filmsondemand:view'] = 'View the Films On Demand repository';
+$string['search'] = 'Search Films On Demand';
+$string['customMessage'] = 'Your request cannot be validated.  Please contact Films On Demand support for assistance and verify your credentials are still valid.';
